f32a808a7d619e57e1a9fb8be55d1e80614c5c62
nitesh-repos <niteshnanotkar@gmail.com>
2020-11-10 00:34:41 +0530

    arrange string

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+def findWord(matchcase):
+    arr = []
+    for lcase in matchcase:
+        a,b = lcase[0],lcase[-1]
+        if a in arr:
+            idx = arr.index(a)
+            if len(arr)-1 != idx:
+                arr = arr[:idx]+[b]+arr[idx:]
+            else:
+                arr.append(b)
+            print(arr)
+        elif b in arr:
+            idx = arr.index(b)
+            if len(arr) - 1 != idx:
+                arr = arr[:idx-1] + [a] + arr[idx-1:]
+            else:
+                arr.append(a)
+            print(arr)
 
-def print_hello(text):
-    print(text)
-
+        if a not in arr and b not in arr:
+            arr.append(a)
+            arr.append(b)
+            print(arr)
+    print(arr)
 if __name__ == '__main__':
-    print_hello('Hello World')
+    findWord(["P>E", "E>R", "R>U"])
+    findWord(["I>N", "A>I", "P>A", "S>P"])
