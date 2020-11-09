4286e80ff9f2009568892f29b3ec88b944c284db
nitesh-repos <niteshnanotkar@gmail.com>
2020-11-10 00:43:56 +0530

    arrange string done

--- main.py
+++ main.py
@@ -12,15 +12,23 @@ def findWord(matchcase):
         elif b in arr:
             idx = arr.index(b)
             if len(arr) - 1 != idx:
-                arr = arr[:idx-1] + [a] + arr[idx-1:]
+                # print(arr,idx)
+                # print(arr[:idx - 1],[a],arr[idx-1:])
+                if idx - 1 > 0:
+                    # print(f'{idx} - 1 > 0')
+                    arr = arr[:idx-1] + [a] + arr[idx-1:]
+                else:
+                    # print("index n")
+                    # print([a] + arr[:])
+                    arr = [a] + arr[:]
             else:
                 arr.append(a)
-            print(arr)
+            # print(arr)
 
         if a not in arr and b not in arr:
             arr.append(a)
             arr.append(b)
-            print(arr)
+            # print(arr)
     print(arr)
 if __name__ == '__main__':
     findWord(["P>E", "E>R", "R>U"])
